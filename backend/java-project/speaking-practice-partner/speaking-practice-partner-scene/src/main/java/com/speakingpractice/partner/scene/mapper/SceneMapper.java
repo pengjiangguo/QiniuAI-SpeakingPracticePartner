7203f23369199022387693aa574fb83d3412d62f
pjg <1593353617@qq.com>
2026-06-07 14:14:32 +0800

    refactor(backendSpringBoot):重构springboot多模块架构\

---
+++ backend/java-project/speaking-practice-partner/speaking-practice-partner-scene/src/main/java/com/speakingpractice/partner/scene/mapper/SceneMapper.java
@@ -0,0 +1,13 @@
+package com.speakingpractice.partner.scene.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.speakingpractice.partner.scene.domain.entity.Scene;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 场景Mapper
+ */
+@Mapper
+public interface SceneMapper extends BaseMapper<Scene> {
+
+}
